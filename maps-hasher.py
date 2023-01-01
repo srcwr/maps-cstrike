e77e0696af920f2e5db89a6dc5a4dd22808c1ac9
rtldg <rtldg@protonmail.com>
2023-01-01 12:05:35 +0000

    more changes for wip gb

--- maps-hasher.py
+++ maps-hasher.py
@@ -15,7 +15,7 @@ mapsfolder = "../todo"
 if os.path.exists(csvname) and os.path.getsize(csvname) > 50:
     raise Exception("DONT OVERWRITE THAT CSV!")
 
-with open(csvname, "w", newline="") as csvfile:
+with open(csvname, "w", newline="", encoding="utf-8") as csvfile:
     mycsv = csv.writer(csvfile)
     mycsv.writerow(["mapname","filesize","filesize_bz2","sha1","note"])
     for filename in glob.glob(mapsfolder + "/**/*.bsp", recursive=True):
@@ -23,16 +23,25 @@ with open(csvname, "w", newline="") as csvfile:
         if S_ISDIR(statttt.st_mode):
             continue
         filesize = statttt.st_size
+        if filesize == 0:
+            with open("shit.txt", "a", encoding="utf-8") as shit:
+                shit.write(f"==== empty file {filename}\n")
+            print(f"==== empty file {filename}")
+            continue
         with open(filename, "rb") as f:
             mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
             vbspver = mm.read(5)
             if vbspver != b'VBSP\x13' and vbspver != b'VBSP\x14':
+                thing = ""
                 if vbspver == b'VBSP\x15':
-                    print("==== skipping CS:GO map " + filename)
+                    thing = "==== skipping CS:GO map " + filename
                 elif vbspver[:4] == b'\x1E\x00\x00\x00' or vbspver[:4] == b'\x1D\x00\x00\x00':
-                    print("==== skipping GoldSrc map? " + filename)
+                    thing = "==== skipping GoldSrc map? " + filename
                 else:
-                    print("==== not a CS:S map? " + filename)
+                    thing = "==== not a CS:S map? " + filename
+                print(thing)
+                with open("shit.txt", "a", encoding="utf-8") as shit:
+                    shit.write(thing + "\n")
                 continue
             mm.seek(0)
             digest = hashlib.sha1(mm).hexdigest()
