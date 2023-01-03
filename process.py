48b2354af0bd5c89d014cb9dbe95b4fa31186d8b
rtldg <rtldg@protonmail.com>
2023-01-03 12:43:35 +0000

    more megabytes and gamebanana page links

--- process.py
+++ process.py
@@ -14,15 +14,20 @@ cur = conn.cursor()
 cur.executescript("""
 DROP TABLE IF EXISTS maps_unfiltered;
 DROP TABLE IF EXISTS maps_canon;
+DROP TABLE IF EXISTS gamebanana;
 
 CREATE TABLE maps_unfiltered (mapname TEXT NOT NULL, filesize INT NOT NULL, filesize_bz2 INT NOT NULL, sha1 TEXT NOT NULL);
 CREATE TABLE maps_canon (mapname TEXT NOT NULL, filesize INT NOT NULL, filesize_bz2 INT NOT NULL, sha1 TEXT NOT NULL);
+CREATE TABLE gamebanana (sha1 TEXT NOT NULL, gamebananaid INT NOT NULL, gamebananafileid INT NOT NULL);
 
 CREATE INDEX mapnameu ON maps_unfiltered(mapname);
 CREATE INDEX sha1m on maps_unfiltered(sha1);
 CREATE INDEX mapnamec ON maps_canon(mapname);
 CREATE INDEX sha1c on maps_canon(sha1);
+CREATE INDEX sha1g on gamebanana(sha1);
 """)
+
+gamebanana = {}
 
 unique = set()
 for filename in glob.glob("unprocessed/*.csv"):
@@ -31,7 +36,13 @@ for filename in glob.glob("unprocessed/*.csv"):
         for line in cr:
             if line[0] == "mapname":
                 continue
-            unique.add(tuple([x.lower() for x in line][:4])) # :4 for anti-note
+            thing = [x.lower() for x in line]
+            if len(thing) > 4:
+                # path & maybe gamebanana path...
+                splits = thing[4].split('_')
+                if splits[0].isdigit() and splits[1].isdigit(): # might have false positives...
+                    gamebanana[thing[3]] = (int(splits[0]), int(splits[1]))
+            unique.add(tuple(thing[:4]))
 
 unfiltered = set(unique)
 for filename in glob.glob("filters/*.csv"):
@@ -40,13 +51,14 @@ for filename in glob.glob("filters/*.csv"):
         for line in cr:
             if line[0] == "mapname":
                 continue
-            unique.add(tuple([x.lower() for x in line]))
+            unique.remove(tuple([x.lower() for x in line]))
             #if line == "mapname,filesize,filesize_bz2,sha1\n":
             #    continue
             #unique.remove(line.lower().strip())
 
 cur.executemany("INSERT INTO maps_unfiltered VALUES(?,?,?,?);", unfiltered)
 cur.executemany("INSERT INTO maps_canon VALUES(?,?,?,?);", unique)
+cur.executemany("INSERT INTO gamebanana VALUES(?,?,?);", [(a,b,c) for a, (b, c) in gamebanana.items()])
 
 with open("canon.csv", encoding="utf-8") as f:
     #things = [[x.lower().strip() for x in line] for line in csv.reader(f)] # also newline='' in open
@@ -62,18 +74,24 @@ def write_mini(filename, content):
             g.write(content)
         h.write(content)
 
-def create_thing(table, outfilename, canon):
+def create_thing(table, outfilename, canon, title):
     res = cur.execute(f"SELECT COUNT(*), SUM(s) FROM (SELECT SUM(filesize) s FROM {table} GROUP BY sha1);").fetchone()
 
     with open("index_top.html", encoding="utf-8") as f:
-        index_html = f.read() + """
-        <h1>BORN TO DIE</h1>
-        <h2>WORLD IS A FUCK</h2>
-        <h3>鬼神 Kill Em All {}</h3>
-        <h3>I am trash man</h3>
-        <h3>{:,} DEAD BYTES</h3>
+        index_html = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>fastdl.me {}</title>
+        """.format(title) + f.read() + """
+        <h1>fastdl.me {}</h1>
+        <h2><a href="https://fastdl.me">homepage</a></h2>
+        <h3>Number of maps: {}</h3>
+        <h3>Unpacked size: {:,} BYTES</h3>
         <h4>(sorting is slow... you have been warned...)</h4>
-        """.format(res[0], res[1])
+        """.format(title, res[0], res[1])
 
     index_html += """
     <table id="list" class="sortable">
@@ -83,12 +101,18 @@ def create_thing(table, outfilename, canon):
     <th style="width:5%">Hash</th>
     <th style="width:5%">Size bsp</th>
     <th style="width:5%">Size bz2</th>
+    <th style="width:5%">Gamebanana page</th>
     </tr>
     </thead>
     <tbody>
     """
 
-    for row in cur.execute(f"SELECT mapname, filesize, filesize_bz2, sha1 FROM {table} ORDER BY mapname;").fetchall():
+    for row in cur.execute(f"SELECT mapname, filesize, filesize_bz2, m.sha1, gamebananaid FROM {table} m LEFT JOIN gamebanana g ON g.sha1 = m.sha1 ORDER BY mapname;").fetchall():
+        gbid = row[4]
+        if gbid == None:
+            gbid = ""
+        else:
+            gbid = f'<td><a href="https://gamebanana.com/mods/{gbid}">{gbid}</a></td>'
         if canon:
             index_html += """
             <tr>
@@ -96,20 +120,23 @@ def create_thing(table, outfilename, canon):
             <td>{}</td>
             <td>{}</td>
             <td>{}</td>
+            {}
             </tr>
-            """.format(html.escape(row[0]), html.escape(row[0]), row[3], row[1], row[2])
+            """.format(html.escape(row[0]), html.escape(row[0]), row[3], row[1], row[2], gbid)
         else:
+            #<td><a href="#">{}</a></td>
             index_html += """
             <tr>
-            <td><a href="#">{}</a></td>
+            <td>{}</td>
             <td><a href="{}.bsp.bz2" download>{}</a></td>
             <td>{}</td>
             <td>{}</td>
+            {}
             </tr>
-            """.format(html.escape(row[0]), row[3], row[3], row[1], row[2])
+            """.format(html.escape(row[0]), row[3], row[3], row[1], row[2], gbid)
 
     with open("index_bottom.html", encoding="utf-8") as f:
         write_mini(f"processed/{outfilename}", index_html + f.read())
 
-create_thing("maps_unfiltered", "hashed/index.html", False)
-create_thing("maps_canon", "maps/index.html", True)
+create_thing("maps_unfiltered", "hashed/index.html", False, "hashed/unfiltered maps")
+create_thing("maps_canon", "maps/index.html", True, "canon/filtered maps")
