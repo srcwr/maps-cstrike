f5219aa86d940d3e6fe0235e9a88afaa26ed719f
rtldg <rtldg@protonmail.com>
2023-01-07 01:41:49 +0000

    fix 404

--- fastdlsite/main.py
+++ fastdlsite/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,g
+from flask import Flask,redirect,g,abort
 import sqlite3
 
 app = Flask(__name__)
@@ -20,8 +20,9 @@ def AAAAA(mapname):
     cur = get_db().cursor()
     cur.execute("SELECT sha1, MAX(filesize_bz2) fbz2 FROM maps_canon WHERE mapname = ? LIMIT 1", (mapname,))
     res = cur.fetchone()
-    if res == None:
-        abort(404)
+    if res == None or res[0] == None:
+        #abort(404)
+        return "", 404
     maphash = res[0]
     return redirect(f"https://mainr2.x.fastdl.me/hashed/{maphash}.bsp.bz2", code=302)
 
