b417ce0802e9d1948c8d7e9f3669a00ce2e699d7
rtldg <rtldg@protonmail.com>
2023-08-11 11:03:29 +0000

    maybe working gamebanana-automatic

---
+++ gamebanana-automatic.py
@@ -0,0 +1,147 @@
+from datetime import datetime
+import sys
+import importlib
+import traceback
+import logging
+import os
+import time
+from threading import Thread
+import shutil
+import csv
+from pathlib import Path
+import requests
+
+maps_hasher = importlib.import_module("maps-hasher")
+
+sys.path.append("../gamebanana-things")
+# this is meh... if only I'd .replace('-', '_')...
+gamebanana_itemizer = importlib.import_module("gamebanana-itemizer")
+gamebanana_pages = importlib.import_module("gamebanana-pages")
+import peeker
+
+
+os.environ["GIT_COMMITTER_NAME"] = "srcwrbot"
+os.environ["GIT_COMMITTER_EMAIL"] = "bot@srcwr.com"
+WEBHOOKURL = open("secretwebhook").read().strip()
+PURGETOKEN = open("secretpurge").read().strip()
+
+def webhook(doping, msg):
+    data = {
+        "content": ("<&871096527202947094> " if doping else "") + msg,
+        "username": "autogb",
+        "embeds": [],
+    }
+    result = requests.post(WEBHOOKURL, json=data)
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("Payload delivered successfully, code {}.".format(result.status_code))
+
+def log_error(error):
+    print(error)
+    with open("shit2.txt", "a", encoding="utf-8") as shit:
+        shit.write(error + "\n")
+    webhook(True, error)
+
+def purge_cloudflare_cache():
+    data = {"purge_everything": True,}
+    result = requests.post("https://api.cloudflare.com/client/v4/zones/1aa75e18589c3649abe7da1eb740bf46/purge_cache", json=data, headers={"Authorization": "Bearer "+PURGETOKEN})
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("Payload delivered successfully, code {}.".format(result.status_code))
+    pass
+
+def rsync_hashed():
+    os.system("start /wait cmd /c ..\\cwrsync_6.2.7_x64_free\\cwrsync.cmd") # lol... wsl rsync spins forever so....
+
+def maps_cstrike_more(now):
+    os.system("start /wait cmd /c ..\\maps-cstrike-more\\auto.cmd "+now) # lol...
+
+def peeker_callback(arg):
+    webhook(True, "new download at https://gamebanana.com/mods/"+arg.split('_')[0])
+
+first = True
+while True:
+    if not first:
+        time.sleep(60 * 5)
+    first = False
+
+    today = datetime.today()
+    now = today.strftime('%Y%m%d%H%M')
+    now4csv = today.strftime('%Y-%m-%d %H:%M')
+
+    try:
+        gamebanana_pages.main("../gamebanana-things")
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        continue
+    gamebanana_itemizer.main("../gamebanana-things")
+    os.system("git -C ../gamebanana-things add gamebanana-items")
+
+    try:
+        new_items = peeker.main("../gamebanana-things", peeker_callback)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        log_error(True, "peeker failed... restart me when you can...")
+        break
+        #continue
+
+    if len(new_items) < 1:
+        continue
+
+    os.system(f'git -C ../gamebanana-things commit --author="srcwrbot <bot@srcwr.com>" -m "{now} -automatic gamebanana"')
+    #os.system('git -C ../gamebanana-things push originbot")
+
+    #new_items = ["0_0_xbhop_badges.7z"]
+    for item in new_items:
+        noext = Path(item).stem
+        status = os.system(f"7z x -y ../gamebanana-scrape/{item} -o../todo-auto/{noext}")
+        if status != 0:
+            log_error(True, f"failed to extract {item}...")
+
+    newly_hashed = maps_hasher.main("unprocessed/gamebanana-x-automatic.csv", True, "../todo-auto", False, False)
+    newly_hashed.sort()
+
+    thread_rsync_hashed = Thread(target=rsync_hashed)
+    thread_maps_cstrike_more = Thread(target=maps_cstrike_more, args=(now,))
+    thread_rsync_hashed.start()
+    thread_maps_cstrike_more.start()
+
+    if len(newly_hashed) > 0:
+        recently_added = []
+        with open("recently_added.csv", newline='', encoding="utf-8") as f:
+            recently_added = [line for line in csv.reader(f)]
+        recently_added.pop(0) # remove "mapname,filesize,filesize_bz2,sha1,note,recently_added_note,datetime"
+        for item in reversed(newly_hashed):
+            recently_added.insert(0, item+["automated upload",now4csv])
+        with open("recently_added.csv", "w", newline='', encoding="utf-8") as f:
+            mycsv = csv.writer(f)
+            mycsv.writerow(["mapname","filesize","filesize_bz2","sha1","note","recently_added_note","datetime"])
+            for item in recently_added:
+                mycsv.writerow(item)
+
+    os.system("git add recently-added.csv unprocessed/gamebanana-automatic.csv")
+    os.system(f'git commit --author="srcwrbot <bot@srcwr.com>" -m "{now} - automatic gamebanana"')
+
+    status = os.system("python process.py") # assume no exceptions... lol...
+    if status != 0:
+        log_error(True, "process.py failed... restart me when you can...")
+
+    thread_rsync_hashed.join()
+
+    if status == 0:
+        os.system("start /wait cmd /c ..\\cwrsync_6.2.7_x64_free\\transfer.cmd") # lol...
+        purge_cloudflare_cache()
+        os.system("git push originbot")
+
+    thread_maps_cstrike_more.join()
+    
+    if status == 0:
+        print("\n\ndone! looping again...")
+    else:
+        break
--- maps-hasher.py
+++ maps-hasher.py
@@ -12,80 +12,97 @@ from pathlib import Path
 from stat import *
 from datetime import datetime
 
-csvname = "unprocessed/misc3.csv"
-mapsfolder = "../todo-gb"
-timestampFixer = False
-skipExistingHash = False
-
-if os.path.exists(csvname) and os.path.getsize(csvname) > 50:
-    raise Exception("DONT OVERWRITE THAT CSV!")
-
 def print_and_to_shit(s):
     print(s)
     with open("shit.txt", "a", encoding="utf-8") as shit:
         shit.write(s + "\n")
 
-with open(csvname, "w", newline="", encoding="utf-8") as csvfile:
-    mycsv = csv.writer(csvfile)
-    mycsv.writerow(["mapname","filesize","filesize_bz2","sha1","note"])
-    for filename in glob.iglob(mapsfolder + "/**/*.bsp", recursive=True):
-        statttt = os.stat(filename)
-        if S_ISDIR(statttt.st_mode):
-            continue
-        filesize = statttt.st_size
-        if filesize == 0:
-            print_and_to_shit(f"==== empty file {filename}")
-            continue
-        with open(filename, "rb") as f:
-            mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-            vbspver = mm.read(5)
-            if vbspver != b'VBSP\x13' and vbspver != b'VBSP\x14':
-                thing = ""
-                if vbspver == b'VBSP\x15':
-                    thing = "==== skipping CS:GO map " + filename
-                elif vbspver == b'VBSP\x19':
-                    thing = "==== skipping Momentum Mod / Strata Source map " + filename
-                elif vbspver[:4] == b'\x1E\x00\x00\x00' or vbspver[:4] == b'\x1D\x00\x00\x00':
-                    thing = "==== skipping GoldSrc map? " + filename
-                else:
-                    thing = "==== not a CS:S map? " + filename
-                print_and_to_shit(thing)
+def dumb_name(m):
+    return m.strip().replace('.', '_')
+
+def main(csvname, append, mapsfolder, timestampFixer, skipExistingHash):
+    if not append and os.path.exists(csvname) and os.path.getsize(csvname) > 50:
+        raise Exception("DONT OVERWRITE THAT CSV!")
+
+    existing_names = {}
+    if append:
+        with open("processed/main.fastdl.me/hashed_index.html.csv", newline='', encoding="utf-8") as f:
+            cr = csv.reader(f)
+            for line in cr:
+                existing_names[dumb_name(line[0])] = True
+
+    newly_hashed = []
+    with open(csvname, ("a" if append else "w"), newline="", encoding="utf-8") as csvfile:
+        mycsv = csv.writer(csvfile)
+        if not append:
+            mycsv.writerow(["mapname","filesize","filesize_bz2","sha1","note"])
+        for filename in glob.iglob(mapsfolder + "/**/*.bsp", recursive=True):
+            statttt = os.stat(filename)
+            if S_ISDIR(statttt.st_mode):
                 continue
-            mm.seek(0)
-            digest = hashlib.sha1(mm).hexdigest()
-            #print("Hash {} -- {}".format(digest, filename))
-            renameto = "../hashed/" + digest + ".bsp"
-            exists = os.path.exists(renameto)
-            if not exists:
-                """
-                if timestampFixer:
-                    print("wtf bad??? {} {}".format(filename, renameto))
-                    mm.close()
-                    continue
-                """
-                print("copying new! {} -> {}".format(filename, renameto))
-                shutil.copy2(filename, renameto)
-                with bz2.open(renameto + ".bz2", "wb") as fbz2:
-                    mm.seek(0)
-                    unused = fbz2.write(mm)
-                shutil.copystat(renameto, renameto+".bz2")
-            mm.close()
-            if exists:
-                if os.stat(renameto).st_size != filesize:
-                    print_and_to_shit(f"??????? HASH COLLISION WOW {digest} {filename}")
-                    continue
-                mtimeThis = os.path.getmtime(filename)
-                mtimeHashed = os.path.getmtime(renameto)
-                if mtimeThis < mtimeHashed:
-                    if timestampFixer:
-                        print("timestamping {} from {} to {}".format(digest, datetime.utcfromtimestamp(mtimeHashed), datetime.utcfromtimestamp(mtimeThis)))
-                        os.utime(renameto, (mtimeThis, mtimeThis))
-                        os.utime(renameto+".bz2", (mtimeThis, mtimeThis))
-                        continue
+            filesize = statttt.st_size
+            if filesize == 0:
+                print_and_to_shit(f"==== empty file {filename}")
+                continue
+            with open(filename, "rb") as f:
+                mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+                vbspver = mm.read(5)
+                if vbspver != b'VBSP\x13' and vbspver != b'VBSP\x14':
+                    thing = ""
+                    if vbspver == b'VBSP\x15':
+                        thing = "==== skipping CS:GO map " + filename
+                    elif vbspver == b'VBSP\x19':
+                        thing = "==== skipping Momentum Mod / Strata Source map " + filename
+                    elif vbspver[:4] == b'\x1E\x00\x00\x00' or vbspver[:4] == b'\x1D\x00\x00\x00':
+                        thing = "==== skipping GoldSrc map? " + filename
                     else:
-                        print("older timestamp for {} from {} -- {} -> {}!".format(digest, filename, datetime.utcfromtimestamp(mtimeHashed), datetime.utcfromtimestamp(mtimeThis)))
-            if exists and skipExistingHash:
-                continue
-            filesize_bz2 = os.stat(renameto + ".bz2").st_size
-            pp = Path(filename)
-            mycsv.writerow([pp.stem,filesize,filesize_bz2,digest,str(pp.parent).replace("\\", "/").replace(mapsfolder+"/", "")])
+                        thing = "==== not a CS:S map? " + filename
+                    print_and_to_shit(thing)
+                    continue
+                mm.seek(0)
+                digest = hashlib.sha1(mm).hexdigest()
+                #print("Hash {} -- {}".format(digest, filename))
+                renameto = "../hashed/" + digest + ".bsp"
+                exists = os.path.exists(renameto)
+                if not exists:
+                    """
+                    if timestampFixer:
+                        print("wtf bad??? {} {}".format(filename, renameto))
+                        mm.close()
+                        continue
+                    """
+                    print("copying new! {} -> {}".format(filename, renameto))
+                    shutil.copy2(filename, renameto)
+                    with bz2.open(renameto + ".bz2", "wb") as fbz2:
+                        mm.seek(0)
+                        unused = fbz2.write(mm)
+                    shutil.copystat(renameto, renameto+".bz2")
+                mm.close()
+                if exists:
+                    if os.stat(renameto).st_size != filesize:
+                        print_and_to_shit(f"??????? HASH COLLISION WOW {digest} {filename}")
+                        continue
+                    mtimeThis = os.path.getmtime(filename)
+                    mtimeHashed = os.path.getmtime(renameto)
+                    if mtimeThis < mtimeHashed:
+                        if timestampFixer:
+                            print("timestamping {} from {} to {}".format(digest, datetime.utcfromtimestamp(mtimeHashed), datetime.utcfromtimestamp(mtimeThis)))
+                            os.utime(renameto, (mtimeThis, mtimeThis))
+                            os.utime(renameto+".bz2", (mtimeThis, mtimeThis))
+                            continue
+                        else:
+                            print("older timestamp for {} from {} -- {} -> {}!".format(digest, filename, datetime.utcfromtimestamp(mtimeHashed), datetime.utcfromtimestamp(mtimeThis)))
+                if exists and skipExistingHash:
+                    continue
+                filesize_bz2 = os.stat(renameto + ".bz2").st_size
+                pp = Path(filename)
+                row = [pp.stem,filesize,filesize_bz2,digest,str(pp.parent).replace("\\", "/").replace(mapsfolder+"/", "")]
+                if append and dumb_name(row[0]) in existing_names:
+                    row[0] = "#" + row[0]
+                if not exists:
+                    newly_hashed.append(row)
+                mycsv.writerow(row)
+    return newly_hashed
+
+if __name__ == "__main__":
+    main("unprocessed/misc3.csv", False, "../todo-gb", False, False)
