6f699342feb89c08b5cf7a9ce2e5b6ad2066b9cc
rtldg <rtldg@protonmail.com>
2023-10-12 09:02:59 +0000

    reusable things for me

--- maps-hasher.py
+++ maps-hasher.py
@@ -118,3 +118,6 @@ def main(csvname, automatic, mapsfolder, timestampFixer, skipExistingHash):
 
 if __name__ == "__main__":
     main("unprocessed/misc3.csv", False, "../todo-gb", False, False)
+    #main("unprocessed/moxx-terabox-unique.csv", False, "F:/terabox/cstrike_all/a", False, True)
+    #main("unprocessed/moxx-terabox-all.csv", False, "F:/terabox/cstrike_all/a", True, False)
+    #main("unprocessed/moxx-terabox-archives.csv", False, "F:/terabox/cstrike_all/archives", True, False)
