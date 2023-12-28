a23f248f5566704d6ed9042ad210941ad8a48d19
rtldg <rtldg@protonmail.com>
2023-12-28 22:29:10 +0000

    ensure db is opened read-only for website

--- fastdlsite/fastdlpy/main.py
+++ fastdlsite/fastdlpy/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect("/fastdl/maps.db") # hardcoding...
+        db = g._database = sqlite3.connect("file:/fastdl/maps.db?mode=ro", uri=True) # hardcoding...
     return db
 
 @app.teardown_appcontext
