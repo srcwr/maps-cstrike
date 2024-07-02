cafeb6a5a50bdbb5b2524e701fdf2142e976a923
rtldg <rtldg@protonmail.com>
2024-07-02 11:51:18 +0000

    edit the check.fastdl.me form slightly

--- fastdlsite/check.fastdl.me/functions/form.js
+++ fastdlsite/check.fastdl.me/functions/form.js
@@ -5,6 +5,10 @@
 export async function onRequestPost(ctx) {
     const now  = new Date().toISOString().replace('T', ' ').slice(0, -5); // lol
 
+    let text = await ctx.request.text();
+    if (text.length < 5 || text.length > 1800)
+        return new Response(null, {status: 500});
+
     const whresp = await fetch(ctx.env.WEBHOOKURL, {
         method: 'POST',
         headers: {
@@ -14,7 +18,7 @@ export async function onRequestPost(ctx) {
         body: JSON.stringify({
             username: "check.fastdl.me",
             avatar_url: "",
-            content: now /*+ ' - ' + ctx.request.cf.country ' - ' + ctx.request.headers.get("X-Real-IP")*/ + '\n```\n' + (await ctx.request.text()) + '\n```',
+            content: now /*+ ' - ' + ctx.request.cf.country ' - ' + ctx.request.headers.get("X-Real-IP")*/ + '\n```\n' + text + '\n```',
             tts: false,
             flags: 4, // SUPPRESS_EMBEDS
             allowed_mentions: {
@@ -24,11 +28,11 @@ export async function onRequestPost(ctx) {
     });
 
     if (!whresp.ok)
-        return new Response('{}', {status: 500});
+        return new Response(null, {status: 500});
 
     let submissions = await ctx.env.CHECK_FASTDL_BUCKET.get("S U B M I S S I O N S.html");
     await ctx.env.CHECK_FASTDL_BUCKET.put("S U B M I S S I O N S.html",
         `<li>${now}<br><i>Not yet viewed</i></li>\n` + await submissions.text())
 
-    return new Response('{"abc": 123}');
+    return new Response(/* defaults to 200 */);
 }
