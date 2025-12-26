b87083af9cd8b1324cd6b26ed7b9796fc0e3a64e
rtldg <rtldg@protonmail.com>
2025-12-26 22:00:35 +0000

    formpy

---
+++ fastdlsite/check.fastdl.me-formpy/main.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: WTFPL
+
+import os
+from time import gmtime, strftime
+
+from flask import Flask,redirect,g,request,jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+@app.post(os.environ["WEBHOOKPATH"])
+def AAAAA():
+    j = request.get_json()
+    with open(strftime("/date/forms/%Y%m%d %H%M%S.txt"), encoding="utf-8") as f:
+        f.write(j["content"])
+    return jsonify({"yip": "pie"}), 200
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=55154)
