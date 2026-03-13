4299c6666bff8a50d7f87a40021f0ba059d081c3
rtldg <rtldg@protonmail.com>
2026-03-13 08:35:53 +0000

    I refuse to write a descriptive commit message

--- update-ksf.surf_maps.sh
+++ update-ksf.surf_maps.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 cd "$(dirname "$0")"/unprocessed
 
-(grep -hoP '\/files\/.*?\.zip' <(curl --proxy socks5h://ca-mtr-wg-socks5-302.relays.mullvad.net:1080 https://ksf.surf/maps) | sort -u | sed 's|/files/||; s|\.zip$||') > ksf.surf_maps-list.txt
+(grep -hoP '\/files\/.*?\.zip' <(curl --fail --proxy socks5h://ca-mtr-wg-socks5-302.relays.mullvad.net:1080 https://ksf.surf/maps) | sort -u | sed 's|/files/||; s|\.zip$||') > ksf.surf_maps-list.txt
 
 git reset
 git add ksf.surf_maps-list.txt
